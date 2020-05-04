e26f975a48b14531b0486c26f5c67c171107fe5d
Ian Armour <ianmarmour@gmail.com>
2020-05-03 21:09:20 -0700

    Added new automatically scaling ap panel.
    Added new automatically scaling ap panel.

--- Assets/Scripts/UI/PlayerMenu/ActionPoints/ActionPoints.cs
+++ Assets/Scripts/UI/PlayerMenu/ActionPoints/ActionPoints.cs
@@ -6,7 +6,10 @@ using System.Linq;
 
 public class ActionPoints : MonoBehaviour
 {
-	public EncounterState encounterStateRef = default;
+	[SerializeField]
+    APPrefab APPrefab = default;
+
+    public EncounterState encounterStateRef = default;
 
     public GameObject ParentPanel;
 
@@ -18,6 +21,31 @@ public class ActionPoints : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {	
+    {
+        Deviant.Entity activeEntity = encounterStateRef.encounter.ActiveEntity;
+        var actionPoints = GameObject.FindGameObjectsWithTag("ui_ap");
+        
+        if(actionPoints.Length < activeEntity.Ap)
+        {
+            var numberOfApToAdd =  activeEntity.Ap - actionPoints.Length;
+
+            for (var i = 0; i < numberOfApToAdd; i++)
+            {
+                APPrefab ap = Instantiate(APPrefab);
+                ap.transform.SetParent(transform, false);
+                ap.transform.gameObject.tag = "ui_ap";
+                ap.transform.SetParent(ap.GetComponentInParent<VerticalLayoutGroup>().transform, true);
+                ap.transform.localPosition = new Vector3(0, 0, 0);
+            }
+        }
+
+        if (actionPoints.Length > activeEntity.Ap)
+        {
+            var numberOfApToRemove = actionPoints.Length - activeEntity.Ap;
+            for (var i = 0; i < numberOfApToRemove; i++)
+            {
+                Destroy(actionPoints[i]);
+            }
+        }
     }
 }
