21aafed32ee65abde46ccf98dde0358d111b98fd
Ian Armour <ianmarmour@gmail.com>
2020-05-04 12:32:18 -0700

    Added automatic rendering scripts for turn order layout
    Added automatic rendering scripts for turn order layout

---
+++ Assets/Scripts/UI/EncounterMenu/TurnOrder.cs
@@ -0,0 +1,39 @@
+﻿using Deviant;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnOrder : MonoBehaviour
+{
+    [SerializeField]
+    TurnOrderUnitPrefab TurnOrderUnitPrefab = default;
+
+    public EncounterState encounterStateRef = default;
+
+    public GameObject ParentPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        encounterStateRef = GameObject.Find("/EncounterState").GetComponent<EncounterState>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var turnOrderUnits = GameObject.FindGameObjectsWithTag("ui_turn_order_unit");
+
+        if (turnOrderUnits.Length < Encounter.ActiveEntityOrderFieldNumber)
+        {
+            for (var i = 0; i < Encounter.ActiveEntityOrderFieldNumber; i++)
+            {
+                TurnOrderUnitPrefab turnOrderUnit = Instantiate(TurnOrderUnitPrefab);
+                turnOrderUnit.transform.SetParent(transform, false);
+                turnOrderUnit.transform.gameObject.tag = "ui_turn_order_unit";
+                turnOrderUnit.transform.SetParent(turnOrderUnit.GetComponentInParent<HorizontalLayoutGroup>().transform, true);
+                turnOrderUnit.transform.localPosition = new Vector3(0, 0, 0);
+            }
+        }
+    }
+}
